784ffd691436ac7fdfe30437cd43c9f981a7261c
SALMA BENBOUKHRIS <salmabenboukhris@gmail.com>
2026-04-03 23:22:13 +0100

    modifie suiviprojetapplication

--- demo/src/main/java/ma/toubkalit/SuiviprojetApplication.java
+++ demo/src/main/java/ma/toubkalit/SuiviprojetApplication.java
@@ -1,13 +1,13 @@
-package ma.toubkalit;
+package ma.toubkalit.suiviprojet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GestionprojetApplication {
+public class SuiviprojetApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GestionprojetApplication.class, args);
+		SpringApplication.run(SuiviprojetApplication.class, args);
 	}
 
 }
